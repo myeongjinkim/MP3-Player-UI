d12a5e6befc3c276f5b45b3d19baa00b9fc27242
60142244_김명진 <kiakd2@gmail.com>
2019-11-11 11:16:05 +0900

    Split screen

---
+++ app/src/main/java/com/example/hw_3/ui/home/HomeFragment.java
@@ -0,0 +1,32 @@
+package com.example.hw_3.ui.home;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.hw_3.R;
+import com.example.hw_3.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    public FragmentHomeBinding binding;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        binding = DataBindingUtil.bind(rootView);
+        binding.setFragment(this);
+        return rootView;
+    }
+}
