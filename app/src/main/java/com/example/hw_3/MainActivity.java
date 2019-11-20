a9d19f86ba8381e52f587614aa2d2996675f81f2
60142244_김명진 <kiakd2@gmail.com>
2019-11-20 23:42:06 +0900

    Fixed portrait screen and rename "최근" to "최근 음악"

--- app/src/main/java/com/example/hw_3/MainActivity.java
+++ app/src/main/java/com/example/hw_3/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_tools)
                 .setDrawerLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
