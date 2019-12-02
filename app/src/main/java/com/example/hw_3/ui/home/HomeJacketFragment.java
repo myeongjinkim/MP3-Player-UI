34f92ee493bff95ebd647e3797be283d2313b672
60142244_김명진 <kiakd2@gmail.com>
2019-12-02 22:01:25 +0900

    make HomeJacketFragment and fragment_home_jacket

--- app/src/main/java/com/example/hw_3/ui/home/HomeJacketFragment.java
+++ app/src/main/java/com/example/hw_3/ui/home/HomeJacketFragment.java
@@ -1,5 +1,48 @@
 package com.example.hw_3.ui.home;
 
-public class HomeJacketFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.hw_3.R;
+import com.example.hw_3.databinding.FragmentHomeJacketBinding;
+
+public class HomeJacketFragment  extends Fragment {
+    private TextView lyricsTextView;
+    private HomeViewModel homeViewModel;
+    public FragmentHomeJacketBinding binding;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home_jacket, container, false);
+        lyricsTextView = rootView.findViewById(R.id.Jacket);
+        homeViewModel = ViewModelProviders.of(getActivity()).get(HomeViewModel.class);
+
+        homeViewModel.getLyrics().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                lyricsTextView.setText(s);
+            }
+        });
+
+        binding = DataBindingUtil.bind(rootView);
+        binding.setFragment(this);
+        return rootView;
+    }
+    public void pressJacket(View view){
+        homeViewModel.LyricsSetting();
+    }
 
 }
